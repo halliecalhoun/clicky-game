d6bd27ff6614302679806e553010690e23dcfd71
Hallie Calhoun <hallieacalhoun@gmail.com>
2019-07-28 18:43:29 -0700

    added styling to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import matches from "./matches.json";
+import "./App.css";
 
 let correctGuesses = 0;
 let bestScore = 0;
--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -6,23 +6,12 @@ function FriendCard(props) {
     <div className="card">
       <div className="img-container">
         <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.setClicked(props.id)} className="card col-md-3">
+     
+      
+      <div onClick={() => props.setClicked(props.id)} className="card col-md-3">
         <img alt={props.name} src={props.image} />
-      </span>
+      </div>
+    </div>
     </div>
   );
 }
